eebaa34c74bfe0e5f446f0c81d95819923447552
Hamza Rami <ramihamza504@gmail.com>
2026-03-15 13:18:45 +0000

    finished cpp06

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -8,27 +8,99 @@ ScalarConverter::~ScalarConverter()
 {
 }
 
-std::string detectType(const std::string& literal)
+int check_float(const std::string& literal)
+{
+    int f = 0;
+    int p = 0;
+    size_t i = 0;
+    int digits = 0;
+    if (literal[literal.size() -1] != 'f')
+        return 0;
+    if (literal[0] == '-' || literal[0] == '+')
+        i++;
+    while (i < literal.size())
+    {
+        if(literal[i] == '.')
+            p++;
+        else if(literal[i] == 'f')
+            f++;
+        else if(!isdigit(literal[i]))
+            return 0;
+        else
+            digits++;
+        if(p > 1 || f > 1)
+            return 0;
+        i++;
+    }
+    if (digits == 0)
+        return 0;
+    return 1;
+}
+
+
+int check_double(const std::string& literal)
+{
+    int p = 0;
+    size_t i = 0;
+    if (literal[0] == '-' || literal[0] == '+')
+        i++;
+    while (i < literal.size())
+    {
+        if(literal[i] == '.')
+            p++;
+        else if(!isdigit(literal[i]))
+            return 0;
+        if(p > 1)
+            return 0;
+        i++;
+    }
+    if (p)
+        return 1;
+    return 0;
+}
+
+int check_int(const std::string& literal)
+{
+    size_t i = 0;
+    if (literal[0] == '-' || literal[0] == '+')
+        i++;
+    while (i < literal.size())
+    {
+        if(!isdigit(literal[i]))
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
+std::string detectType(const  std::string& literal)
 {
     if (literal.size() == 3 && literal[0] == '\'' && literal[literal.size()-1] == '\'')
         return "char";
-    else if (literal.size() == 1 && isalpha(literal[0]))
+    else if (literal.size() == 1 && !isdigit(literal[0]))
         return "char";
     else if (literal == "-inff" || literal == "+inff" || literal == "nanf")
         return "float";
     else if (literal == "-inf" || literal == "+inf" || literal == "nan")
         return "double";
-    else if (literal.size() > 1 && literal[literal.size()-1] == 'f')
-        return "float";
-    else if (literal.find('.') != std::string::npos)
+    else if (check_float(literal))
+            return "float";
+    else if(check_double(literal))
         return "double";
-    else
+    else if(check_int(literal))
         return "int";
+    else
+        return "NON";
 }
 
 void ScalarConverter::convert(const std::string& literal)
 {
     std::cout << std::fixed << std::setprecision(1);
+    if (literal.size() == 0)
+    {
+        std::cout << "type unknown\n";
+        return;
+    }
     std::string type = detectType(literal);
     
     if (type == "char")
@@ -45,16 +117,25 @@ void ScalarConverter::convert(const std::string& literal)
     }
     else if (type == "int")
     {
+    double check = atof(literal.c_str());
+    if (check > INT_MAX || check < INT_MIN)
+    {
+        std::cout << "char: impossible\n";
+        std::cout << "int: impossible\n";
+        std::cout << "float: " << static_cast<float>(check) << "f\n";
+        std::cout << "double: " << check << "\n";
+    }
+    else
+    {
         int a = atoi(literal.c_str());
         if (a >= 32 && a <= 126)
             std::cout << "char: " << static_cast<char>(a) << "\n";
         else
             std::cout << "char: Non displayable\n";
-
         std::cout << "int: " << a << "\n";
-
         std::cout << "float: " << static_cast<float>(a) << "f\n";
         std::cout << "double: " << static_cast<double>(a) << "\n";
+    }
     }
     else if (type == "float")
     {
@@ -66,7 +147,7 @@ void ScalarConverter::convert(const std::string& literal)
         else
             std::cout << "char: Non displayable\n";
 
-        if (std::isnan(a) || std::isinf(a) || a < (float)INT_MIN || a > (float)INT_MAX)
+        if (std::isnan(a) || std::isinf(a) || a < -2147483648.0 || a > 2147483647.0)
             std::cout << "int: impossible\n";
         else
             std::cout << "int: " << static_cast<int>(a) << "\n";
@@ -85,7 +166,7 @@ void ScalarConverter::convert(const std::string& literal)
         else
             std::cout << "char: Non displayable\n";
 
-        if (std::isnan(a) || std::isinf(a) || a < (double)INT_MIN || a > (double)INT_MAX)
+        if (std::isnan(a) || std::isinf(a) || a < -2147483648.0 || a > 2147483647.0)
             std::cout << "int: impossible\n";
         else
             std::cout << "int: " << static_cast<int>(a) << "\n";
@@ -93,4 +174,6 @@ void ScalarConverter::convert(const std::string& literal)
         std::cout << "float: " << static_cast<float>(a) << "f\n";
         std::cout << "double: " << a << "\n";
     }
+    else
+        std::cout << "type unknown\n";
 }
