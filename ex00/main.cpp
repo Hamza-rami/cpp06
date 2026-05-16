ad08843cf7ce5124d509270803652a71e67563a1
Hamza Rami <ramihamza504@gmail.com>
2026-05-16 22:27:20 +0100

    feat: finnished cpp06

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,54 +4,6 @@ int main(int ac, char **av)
 {
     if (ac != 2)
         return 1;
-    float i = 6f;
     ScalarConverter::convert(av[1]);
 }
 
-int main(int ac, char **av)
-{
-    if (ac != 2)
-        return 1;
-    float i = 6f;
-    ScalarConverter::convert(av[1]);
-}
-
-int main(int ac, char **av)
-{
-    if (ac != 2)
-        return 1;
-    float i = 6f;
-    ScalarConverter::convert(av[1]);
-}
-
-int main(int ac, char **av)
-{
-    if (ac != 2)
-        return 1;
-    float i = 6f;
-    ScalarConverter::convert(av[1]);
-}
-
-int main(int ac, char **av)
-{
-    if (ac != 2)
-        return 1;
-    float i = 6f;
-    ScalarConverter::convert(av[1]);
-}
-
-int main(int ac, char **av)
-{
-    if (ac != 2)
-        return 1;
-    float i = 6f;
-    ScalarConverter::convert(av[1]);
-}
-
-int main(int ac, char **av)
-{
-    if (ac != 2)
-        return 1;
-    float i = 6f;
-    ScalarConverter::convert(av[1]);
-}
